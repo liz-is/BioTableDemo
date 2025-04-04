a26b140faf76b8e4569227cf81f6765ad90ee34b
Liz Ing-Simmons (k2474365) <liz.ing-simmons@kcl.ac.uk>
2025-04-04 14:24:12 +0100

    feat: add a second demo table with raw counts per gene

--- app.R
+++ app.R
@@ -1,6 +1,7 @@
 library(shiny)
 library(BioTableModule)
 
+pasilla_counts <- read.csv("data/pasilla_counts.csv", header = TRUE)
 pasilla_df <- read.csv("data/pasilla_condition_treated_results.csv", header = TRUE)
 colnames(pasilla_df) <- c(
   "Gene ID",
@@ -17,16 +18,25 @@ default_columns <- c("Gene ID",
                      "P-value",
                      "Adjusted p-value")
 
-ui <- fluidPage(tableUI(id = "pasilla"))
+ui <- fluidPage(
+  tableUI(id = "pasilla_results"),
+  tableUI(id = "pasilla_counts")
+  )
 
 server <- function(input, output, session) {
   shinyhelper::observe_helpers()
+
   tableServer(
-    "pasilla",
+    "pasilla_results",
     pasilla_df,
     default_cols = default_columns,
     sci_format_cols = setdiff(colnames(pasilla_df), "Gene ID")
   )
+
+  tableServer(
+    "pasilla_counts",
+    pasilla_counts,
+    sci_format_cols = "treated")
 }
 
 shinyApp(ui, server)
